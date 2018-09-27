8564c2f441da8d410426c4dfb62014720895bbf4
elmoknit <elmoknit@gmail.com>
2018-09-27 10:11:23 -0400

    Add copy-webpack-plugin for static folder

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -3,6 +3,12 @@
 const webpack = require('webpack');
 const { VueLoaderPlugin } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+function resolve (dir) {
+    return path.join(__dirname, '..', dir);
+}
 
 module.exports = {
     mode: 'development',
@@ -47,6 +53,11 @@ module.exports = {
             filename: 'index.html',
             template: 'index.html',
             inject: true
-        })
+        }),
+        new CopyWebpackPlugin([{
+            from: resolve('static/img'),
+            to: resolve('dist/static/img'),
+            toType: 'dir'
+        }])
     ]
 }
