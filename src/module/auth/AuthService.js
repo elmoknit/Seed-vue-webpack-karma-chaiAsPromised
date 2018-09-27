5a25bfcd604f4ba2eeccc4027e9c47180299cf49
elmoknit <elmoknit@gmail.com>
2018-09-27 10:33:01 -0400

    Add karma

---
+++ src/module/auth/AuthService.js
@@ -0,0 +1,13 @@
+export default class AuthService {
+
+  constructor () {
+  }
+
+  validateForm (email, password) {
+   //todo
+  }
+
+  login (email, password) {
+    //todo
+  }
+}
---
+++ test/unit/specs/module/auth/AuthService.spec.js
@@ -0,0 +1,11 @@
+import AuthService from '../../../../../src/module/auth/AuthService';
+
+
+describe('AuthService', () => {
+
+  describe('validateForm', () => {
+    it('should return true', () => {
+      //todo
+    });
+  });
+});
